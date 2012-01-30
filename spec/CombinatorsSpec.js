9397ff93b298e5568edc55acda286b06aa2a761d
Diego Echeverri <diegoeche@gmail.com>
2012-01-30 16:36:26 -0500

    adding safeWith method

--- spec/CombinatorsSpec.js
+++ spec/CombinatorsSpec.js
@@ -2,10 +2,32 @@ describe("Combinators", function () {
   it("should create a div element", function () {
     $builder.div("hello").html().should === "hello";
   });
+
   it("should set the id attr if the parameters contain a string starting with '#'", function () {
     $builder.div("#myid", "hello").attr("id").should == "myid"
   });
+
   it("should set the class name if the parameters contain a string starting with '.'", function () {
     $builder.div(".myclass", "hello").attr("class").should == "myclass"
+  });
+});
+
+describe("$builder.safeWith", function () {
+  it("should restore the global values after execution", function () {
+    window.a = 5;
+    $builder.safeWith(function () {}, {a: 4});
+    window.a.should === 5;
+  });
+
+  it("should restore the global-undefined values after execution", function () {
+    window.a = undefined;
+    $builder.safeWith(function () {}, {a: 4});
+    window.a.should === undefined;
+  });
+
+  it("should execute the function in the right global context", function () {
+    $builder.safeWith(function () {
+      return a;
+    }, {a: 4}).should === 4;
   });
 });
--- src/jquery.html.combinators.js
+++ src/jquery.html.combinators.js
@@ -1,21 +1,47 @@
-var tagNames = "a abbr address area article aside audio b base bdo blockquote body br button canvas caption cite code col colgroup command datalist dd del details dfn div dl dt em embed fieldset figcaption figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins keygen kbd label legend li link map mark menu meta meter nav noscript object ol optgroup option output p param pre progress q rp rt ruby s samp script section select small source span strong style sub summary sup table tbody td textarea tfoot th thead time title tr ul var video wbr".split(" ")
+(function  () {
+  var tagNames = "a abbr address area article aside audio b base bdo blockquote body br button canvas caption cite code col colgroup command datalist dd del details dfn div dl dt em embed fieldset figcaption figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins keygen kbd label legend li link map mark menu meta meter nav noscript object ol optgroup option output p param pre progress q rp rt ruby s samp script section select small source span strong style sub summary sup table tbody td textarea tfoot th thead time title tr ul var video wbr".split(" ")
 
-var $builder = {}
+  window.$builder = {};
 
-$.each(tagNames, function  (_, tag) {
-  var builder = function () {
-    var node = $("<" + tag + ">");
-    $.each(arguments, function (_, arg) {
-      if (typeof(arg) === "string") {
-        if (arg[0] === ".") node.addClass(arg.substring(1))
-        else if (arg[0] === "#") node.attr("id", arg.substring(1))
-        else node.append($("<div>").text(arg).html());
-      } else {
-        node.append(arg);
+  $.each(tagNames, function  (_, tag) {
+    var builder = function () {
+      var node = $("<" + tag + ">");
+      $.each(arguments, function (_, arg) {
+        if (typeof(arg) === "string") {
+          if (arg[0] === ".") node.addClass(arg.substring(1));
+          else if (arg[0] === "#") node.attr("id", arg.substring(1));
+          else node.append($("<div>").text(arg).html());
+        } else {
+          node.append(arg);
+        }
+      });
+      return node;
+    }
+    $builder[tag] = builder;
+  });
+
+  // A with-like functionality that creates a backup of the
+  // references in the global object.
+  $builder.safeWith = function(fun, obj) {
+    // Backup references.
+    var globalBackup = {};
+    for(var key in obj) {
+      if(obj.hasOwnProperty(key)) {
+        if(window[key] !== undefined) {
+          globalBackup[key] = window[key];
+        }
+        window[key] = obj[key];
       }
-    });
-    return node;
-  }
-  $builder[tag] = builder;
-})
+    }
+    // Execute function in the right global context.
+    var returnValue = fun();
+    // Restore old references;
+    for(var key in globalBackup) {
+      if(globalBackup.hasOwnProperty(key)) {
+        window[key] = globalBackup[key];
+      }
+    }
+    return returnValue;
+  };
+})();
 
